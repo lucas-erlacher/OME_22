e6574090d8d6f0843f45eb02eeb50b31baea8ddd
Lucas Erlacher <lucaserlacher@gmail.com>
2022-04-14 14:15:38 +0200

    implemented (very) simple mutation

--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 import System.Environment
 import Data.List (sortBy)
+import System.Random
 
 -- types that represent the requirements
 type TeacherToSubjects = (String, [String])
@@ -40,7 +41,7 @@ main = do
     print (show (run 100 10 requirements))
 
 run :: Int -> Int -> Requirements -> SchoolTimetable
-run numEnts numGens reqs  = head (sortTimetables iterationRes)
+run numEnts numGens reqs = head (sortTimetables iterationRes)
     where iterationRes = Main.iterate numGens (generateInitialEnts numEnts reqs)
 
 -- little helper function
@@ -51,7 +52,10 @@ fitness :: SchoolTimetable -> Int
 fitness table = if score == 0 then actualFitness table else (- score)
     where
         score = invalidityScore table
-        actualFitness table = 0  -- TODO: just a placeholder for now
+
+-- TODO: implement the fitness function of our algorithm 
+actualFitness :: SchoolTimetable -> Int
+actualFitness table = 0
 
 -- TODO: return a score of how badly invalid a given timetable is (or 0 if it's valid)
 invalidityScore :: SchoolTimetable -> Int
@@ -64,30 +68,66 @@ iterate n currEnts = Main.iterate (n-1) newEnts
         newEnts = crossOver mutatedEnts
         mutatedEnts = map mutate currEnts
 
--- TODO
+-- TODO: perform all the crossing overs that should happen in a generation
 crossOver :: [SchoolTimetable] -> [SchoolTimetable]
 crossOver ents = ents
 
--- TODO
+-- very simple implementation: a mutation is just swapping two slots in the timetable of a class
 mutate :: SchoolTimetable -> SchoolTimetable
-mutate ent = ent
+mutate = map decideAndPerform
+
+-- decide how many (if any) mutations a given class timetable should recieve (and also perform them)
+decide :: ClassTimetable -> ClassTimetable
+decide = performMuts numMuts   
+    where
+        numMuts = randomRIO (0, 5)  -- let's say (for now) that an entity can recieve 5 mutations at max
+
+-- perform all the mutations that a class timtable should recieve
+performMuts :: Int -> ClassTimetable -> ClassTimetable
+performMuts 0 table = table
+performMuts n table = performMuts n - 1 (performSingleMut table)
+    
+performSingleMut :: ClassTimetable -> Class
+performSingleMut = swap ind1 ind2 
+    where 
+        ind1 = randomRIO (0, 49)
+        ind2 = randomRIO (0, 49)
+
+-- swaps out the two list elements specified by ind1 and ind2 (zero indexed)
+swap :: Int -> Int -> [a] -> [a]
+swap ind1 ind2 table = firstPart ++ [table !! ind2] ++ middlePart ++ [table !! ind1] ++ endPart
+    where
+        firstPart = take ind1 table
+        middlePart = drop (ind1 + 1) (take ind2 table)
+        endPart = drop (ind2 + 1) table
 
 -- very simple implementation: construct initial tables based on ClassToSubjectHours only and if one of them ends up being invalid 
--- (e.g. a teacher teaching 2 classes at the same time) it'll just get a negative fitness score (and will hence get eliminated soon)
+-- (e.g. a teacher teaching 2 classes at the same time) it'll just get a negative fitness score (and will hence get eliminated soon).
+-- if this implementation is too slow (which it probably will) we could run FET for a short time (say 20 seconds) and hope that that
+-- generates a reasonable starting point which we can then refine with the genetic procedure
 generateInitialEnts :: Int -> Requirements -> [SchoolTimetable]
 generateInitialEnts 0 _ = []
 generateInitialEnts n reqs = generateInitialEnt reqs : generateInitialEnts (n-1) reqs
 
+-- generates timetable for an enitre school
 generateInitialEnt :: Requirements -> SchoolTimetable
 generateInitialEnt (teachers, x:xs) = generateInitialTableForClass (snd x) teachers : generateInitialEnt (teachers, xs)
 generateInitialEnt _ = []
 
+-- generated a timetable for a given class
 generateInitialTableForClass :: [(String, Int)] -> [TeacherToSubjects] -> ClassTimetable
-generateInitialTableForClass subjHourList teachers = foldr (\info list -> generateSlots info teachers ++ list) [] subjHourList
+generateInitialTableForClass subjHourList teachers = subjecSlots ++ paddingSlots (50 - length subjectSlots)
+    where subjectSlots = foldr (\info list -> generateSlots info teachers ++ list) [] subjHourList
 
+-- generates a list of slots for a given subject
 generateSlots :: (String, Int) -> [TeacherToSubjects] -> [Slot]
 generateSlots (_, 0) _ = []
 generateSlots (subj, n) teachers = Lesson (findTeacherForSubject subj teachers) subj : generateSlots (subj, n - 1) teachers
+
+-- generates as many free Slots as are needed to fill a school-week (= 50 hours)
+paddingSlots :: Int -> [Slots]
+paddingSlots 0 = []
+paddingSlots n = Free :: paddingSlots (n - 1)
 
 -- implementation assumes that there exists at least one teacher for each subjet
 findTeacherForSubject :: String -> [TeacherToSubjects] -> String
