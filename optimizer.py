028b24ada3ce46ca12424b741a83e4fb9cbfcc2d
Lucas Erlacher <lucaserlacher@gmail.com>
2022-05-10 00:03:51 +0200

    list of lists based implementation

--- optimizer.py
+++ optimizer.py
@@ -1,3 +1,7 @@
+# ASSUMPTIONS:
+# All teachers can in theory teach all subjects
+# There are at least as many teachers as there are classes
+
 import math
 import random
 
@@ -8,38 +12,81 @@ class Optimizer:
         self.muatation_rate = params[2]
         self.elitism_degree = params[3]
 
-    def run(self, reqs):
-        curr_gen_ents = self.__generate_initial_ents(reqs)
+    def run(self, reqs, num_slots, prefered_subjects):
+        teachers = list(map(lambda x: x[0], prefered_subjects))
+        curr_gen_ents = self.__generate_initial_ents(reqs, num_slots, teachers)
         for i in range(self.num_gens):
             old_gen_ents = curr_gen_ents
-            curr_gen_ents = self.__mutate_all(curr_gen_ents)
-            curr_gen_ents = self.__cross_over(curr_gen_ents)
+            curr_gen_ents = self.__mutate_all(curr_gen_ents, num_slots, teachers)
+            curr_gen_ents = self.__cross_over_all(curr_gen_ents)
             # elitism 
             num_old_gen = math.floor(self.num_ents * self.elitism_degree)
             num_curr_gen = self.num_ents - num_old_gen
             sorted_old_gen = old_gen_ents.sort(key=lambda x: self.__fitness(x))
             sorted_curr_gen = curr_gen_ents.sort(key=lambda x: self.__fitness(x))
             curr_gen_ents = sorted_old_gen[0:num_old_gen] + sorted_curr_gen[0:num_curr_gen]
-        print(curr_gen_ents)
+        return curr_gen_ents
 
-    def __generate_initial_ents(reqs):
-        pass
+    def __generate_initial_ents(self, reqs, num_slots, teachers):
+        num_classes = len(reqs)
+        ents = []
+        for i in range(self.num_ents):
+            # fill the school table with subjects
+            school_timetable = []
+            for j in range(num_classes):
+                curr_class_reqs = reqs[j]
+                expanded_reqs = self.__flatten(list(map(self.__expand, curr_class_reqs)))
+                # pad class table with free slots
+                if len(expanded_reqs) < num_slots:
+                    expanded_reqs += "Free" * (num_slots - len(expanded_reqs))
+                random.shuffle(expanded_reqs)
+                school_timetable.append(expanded_reqs)
+            # fill the school table with teachers
+            for i in range(num_slots):
+                random.shuffle(teachers)
+                for j in range(num_classes):
+                    school_timetable[j][i] = (school_timetable[j][i], teachers[j])
+            ents.append(school_timetable)
+        return ents
 
-    def __mutate_all(self, ents):
+    def __expand(req):
+        subj = req[0]
+        num_repetitions = int(req[1])
+        return subj * num_repetitions
+
+    def __flatten(l):
+        res = []
+        for sublist in l:
+            res += sublist
+        return res
+
+    def __mutate_all(self, ents, num_slots, teachers):
         mutated_ents = []
         for ent in ents:
             rand_num = random.uniform(0, 1)
             if rand_num <= self.muatation_rate:
-                mutated_ents.append(self.__mutate_one(ent))
+                mutated_ents.append(self.__mutate_one(ent, num_slots, teachers))
             else:
                 mutated_ents.append(ent)
         return mutated_ents
 
-    def __mutate_one(ent):
-        pass
+    def __mutate_one(ent, num_slots, teachers):
+        num_classes = len(ent)
+        row_num = random.randrange(0, num_slots - 1)
+        random.shuffle(teachers)
+        for j in range(num_classes):
+            ent[j][row_num] = (ent[j][row_num], teachers[j])
 
-    def __cross_over(ents):
-        pass  
+    def __cross_over_all(self, ents):
+        # enforcing even number of ents per generation simplifies this method a bit
+        assert(len(ents) % 2 == 0)
+        random.shuffle(ents)
+        crossed_ents = []
+        for i in range(len(ents)/2):
+            crossed_ents.append(self.__cross_over_two([ents[i], ents[i + 1]]))
+
+    def __cross_over_two(ents):
+        pass
 
     def __fitness(ent):
         pass
--- simple_test.py
+++ simple_test.py
@@ -1,25 +1,9 @@
 from optimizer import Optimizer
 
 if __name__ == "__main__":
-    requirements = ([("Mr Wirz", ["Math"]),("Mrs Rosenberg", ["Biology", "Design Of Digital Circuits"]),("Mr Erlacher", ["Swiss-German"])],[("1A",[("Math", 4), ("Biology", 1), ("Swiss-German", 2)]),("1B", [("Math", 3), ("Biology", 3), ("Swiss-German", 1)])])
-    # READABLE VERSION: 
-    # (
-    #     [
-    #         ("Mr Wirz", ["Math"]),
-    #         ("Mrs Rosenberg", ["Biology", "Design Of Digital Circuits"]), 
-    #         ("Mr Erlacher", ["Swiss-German"])
-    #     ], 
-    #     [
-    #         (
-    #             "1A",
-    #             [("Math", 4), ("Biology", 1), ("Swiss-German", 2)]
-    #         ),
-    #         (
-    #             "1B", 
-    #             [("Math", 3), ("Biology", 3), ("Swiss-German", 1)]
-    #         )
-    #     ]
-    # )
+    class_reqs = [("1A",[("Math", 1), ("Biology", 1), ("Swiss-German", 2)]),("1B", [("Math", 2), ("Design Of Digital Circuits", 1)])]
+    num_slots = 4
+    prefered_subjects = [("Mr. W", ["Math", "Swiss-German"]), ("Mrs. R", ["Biology", "Design Of Digital Circuits"])]
     params = [100, 100, 0.1, 0.1]
     opt = Optimizer(params)
-    print(opt.run(requirements))
+    print(opt.run(class_reqs, num_slots, prefered_subjects))
