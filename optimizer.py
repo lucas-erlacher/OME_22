7933091613dbec7e07ec2266f1a65399644c705a
Lucas Erlacher <lucaserlacher@gmail.com>
2022-05-27 14:29:18 +0200

    this exact code gets 650 out of 768 in 30 minutes

--- optimizer.py
+++ optimizer.py
@@ -7,11 +7,10 @@
 # FITNESS FACTORS
 #   fitness function: teachers dont like gaps (i.e. Free lessons they are assigned to)
 # GENERAL
-#   idea: grow generation_size and mut_rate as you get further (bc innovation is very slow after 400 generations or so)
 #   try more genetic algorithm things such as speciation
 # PERFORMANCE
 #   why the hell is the multiprocessing.Pool getting slower in each generation?
-#   multiprocess more things (e.g. the mapping fitness onto ents)
+#   multiprocess more things (e.g. the mapping of fitness onto ents)
 #   more optimizations (?)
 #   find threshold (in terms of generation size) when to use multiprocessing (provided you fix the slowing down bug)
 # EVALUATION
@@ -29,7 +28,7 @@ class Optimizer:
     def __init__(self, params):
         self.num_gens = params[0]
         self.num_ents = params[1]
-        self.muatation_rate = params[2]
+        self.mutation_rate = params[2]
         self.elitism_degree = params[3]
         self.fitness_cache = dict()
         # for debuging/profiling
@@ -45,6 +44,9 @@ class Optimizer:
         top_ever_ent = []
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
         for i in range(self.num_gens):
+            # increase exploration after a few generations
+            if i >= 300 and self.mutation_rate < 1:
+                self.mutation_rate += 0.01
             a = time.time()
             # copy needed in order for the mutation and crossing not to mess with the old generation
             x = time.time()
@@ -148,212 +150,7 @@ class Optimizer:
         for input in batch:   
             ent = input[0]
             # decide for every ent (= a school timetable) whether or not to shuffle one of its rows
-            if random.randrange(0, 1) < self.muatation_rate:
-                teachers = input[1]
-                rand_num = random.randrange(0, 49)
-                random.shuffle(teachers)
-                for j in range(len(ent)):
-                    ent[j][rand_num] = (ent[j][rand_num][0], teachers[j])
-            res.append(ent) 
-        return res
-
-    def __cross_over_all(self, ents, teachers, prefered_subjects, pool):
-        # enforcing even number of ents per generation simplifies this method a bit
-        num_ents = len(ents)
-        assert(num_ents % 2 == 0)
-        random.shuffle(ents)
-        pairs = []
-        for i in range(int(num_ents / 2)):
-            pairs.append([ents[i], ents[i + 1]])
-        teacher_list = ([teachers] * (math.floor(num_ents / 2)))
-        subjs_list = []
-        for i in range(math.floor(num_ents / 2)):
-            subjs_list.append(prefered_subjects)
-        data = list(zip(pairs, teacher_list, subjs_list))
-        tasks = (np.array_split(data, multiprocessing.cpu_count()))
-        if self.use_multiprocessing:
-            crossed_ents = self.__flatten(self.__flatten(pool.map(self.cross_over_batch, tasks)))
-        else:
-            crossed_ents = self.__flatten(self.__flatten(map(self.cross_over_batch, tasks)))
-        return crossed_ents
-
-    def cross_over_batch(self, batch):
-        res = []
-        for input in batch:
-            ents = input[0]
-            teachers = input[1]
-            prefered_subjects = input[2]
-            parent_1 = ents[0]
-            parent_2 = ents[1]
-            child_1 = []
-            child_2 = []
-            num_classes = len(parent_1)
-            # construct the children
-            for i in range(num_classes):
-                num = random.uniform(0, 1)
-                if num < 0.5: 
-                    child_1.append(parent_1[i])
-                    child_2.append(parent_2[i])
-                else:
-                    child_1.append(parent_2[i])
-                    child_2.append(parent_1[i])
-            child_1 = self.__fix_teacher_conflicts(child_1, teachers, prefered_subjects)
-            child_2 = self.__fix_teacher_conflicts(child_2, teachers, prefered_subjects)
-            res.append([child_1, child_2])
-        return res
-
-    def __fix_teacher_conflicts(self, ent, teachers, prefered_subjects):
-        num_classes = len(ent)
-        num_slots = 50
-        for i in range(num_slots):
-            # copy needed in order for leftover_teachers to be a copy of the full teachers list every time the outermost loop loops
-            leftover_teachers = teachers[:]
-            for j in range(num_classes):
-                self.__remove_if_there(ent[j][i][1], leftover_teachers)
-                for k in range(j + 1, num_classes): 
-                    self.__remove_if_there(ent[k][i][1], leftover_teachers)
-                    if ent[j][i][1] == ent[k][i][1]:
-                        subj = ent[j][i][0]
-                        # try to resolve the conflict using a teacher that is prefered in this subject
-                        qualified = list(filter(lambda x: subj in prefered_subjects[x], leftover_teachers))
-                        if len(qualified) > 0:
-                            picked_teacher = qualified[0]
-                            ent[k][i] = (subj, qualified[0])
-                        else:
-                            if len(leftover_teachers) == 1:
-                                # edge case
-                                picked_teacher = leftover_teachers[0]
-                            else:
-                                picked_teacher = leftover_teachers[random.randrange(0, len(leftover_teachers) - 1)]
-                            ent[k][i] = (subj, picked_teacher)
-                        leftover_teachers.remove(picked_teacher)   
-        return ent
-
-    def __remove_if_there(self, elem, l):
-      try:
-        l.remove(elem)
-        return l
-      except (Exception):
-        return l
-      
-    def __fitness(self, ent, prefered_subjects):
-        if ent == []:
-            return 0
-        # caching
-        if str(ent) in self.fitness_cache:
-            return self.fitness_cache[str(ent)]
-        else:
-            num_classes = len(ent)
-            num_slots = 50
-            score = 0
-            # weights of the different factors that contribute to the fitness of an ent
-            prefered_subject_weight = 1
-            gaps_weight = 0.2
-            # plus points if a teacher teaches a subject he is good at
-            for i in range(num_classes):
-                for j in range(num_slots):
-                    subject = ent[i][j][0]
-                    teacher = ent[i][j][1]
-                    if subject in prefered_subjects[teacher]:
-                        score += prefered_subject_weight * 1
-            # minus points for gaps (i.e. a free in between two lessons or if first lesson of the day is free)
-            # compute for all gaps their length and whether they are followed by a lesson or not 
-            gap_info = []
-            for i in range(num_classes):
-                # for each day in the timetable
-                for k in range(5):
-                    len_counter = 0
-                    # for each slot in that day
-                    for j in range(k * 10, (k + 1) * 10):
-                        if ent[i][j][0] == "Free":
-                            len_counter += 1
-                            # if last slot of the day is free then we need to save the gap and not rely on this being done when the next lesson filled slot comes
-                            if j == ((k + 1) * 10) - 1:
-                                gap_info.append((len_counter, False))
-                        else:
-                            # if this lesson was preceeded by a gap
-                            if not (len_counter == 0):
-                                gap_info.append((len_counter, True))
-                                len_counter = 0
-            # only penlize gaps that are followed by a lesson
-            gap_info = list(filter(lambda x: x[1], gap_info))
-            gap_lens = list(map(lambda x: x[0], gap_info))
-            for gap_len in gap_lens:
-                score -= gaps_weight * gap_len
-            # cache the fitness value of the ent
-            self.fitness_cache[str(ent)] = score
-            return score
-
-    def __print_ent(self, ent):
-        print("START OF ENT")
-        for school_class in ent:
-            for slot in school_class:
-                print(slot, sep=",")
-            print()
-
-    def __print_sorted_fitnesses(self, ents, prefered_subjects):
-        tmp_ents = ents
-        tmp_ents.sort(key=lambda x: self.__fitness(x, prefered_subjects), reverse=True)
-        print(list(map(lambda x: self.__fitness(x, prefered_subjects), tmp_ents)))
-
-        # plot the evolution
-        print("FITNESS OF TOP EVER SEEN ENT: " + str(self.__fitness(top_ever_ent, prefered_subjects)))
-        plt.plot(top_ents_fitnesses, "g", label="top_ent of gen")
-        plt.plot(worst_ents_fitnesses, "r", label="worst_ent of gen")
-        plt.legend(loc="lower right")
-        e = time.time()
-        print("TOTAL RUNTIME: " + str(e - s))
-        plt.show()
-
-    def __generate_initial_ents(self, reqs, teachers):
-        num_slots = 50
-        num_classes = len(reqs)
-        ents = []
-        for i in range(self.num_ents):
-            # fill the school table with subjects
-            school_timetable = []
-            for j in range(num_classes):
-                curr_class_reqs = reqs[j][1]
-                expanded_reqs = self.__flatten(list(map(self.__expand, curr_class_reqs)))
-                # pad class table with free slots
-                if len(expanded_reqs) < num_slots:
-                    expanded_reqs += ["Free"] * (num_slots - len(expanded_reqs))
-                random.shuffle(expanded_reqs)
-                school_timetable.append(expanded_reqs)
-            # fill the school table with teachers
-            for i in range(num_slots):
-                random.shuffle(teachers)
-                for j in range(num_classes):
-                    school_timetable[j][i] = (school_timetable[j][i], teachers[j])
-            ents.append(school_timetable)
-        return ents
-      
-    def __expand(self, req):
-        subj = req[0]
-        num_repetitions = int(req[1])
-        return [subj] * num_repetitions
-
-    def __flatten(self, l):
-        res = []
-        for sublist in l:
-            res += sublist
-        return res
-
-    def __mutate_all(self, ents, teachers, pool):
-        data = list(zip(ents, ([teachers] * len(ents))))
-        tasks = (np.array_split(data, multiprocessing.cpu_count()))
-        if self.use_multiprocessing:
-            mutated_ents = self.__flatten(pool.map(self.mutate_batch, tasks))
-        else: 
-            mutated_ents = self.__flatten(map(self.mutate_batch, tasks))
-        return mutated_ents
-
-    def mutate_batch(self, batch):
-        res = []
-        for input in batch:   
-            ent = input[0]
-            # decide for every ent (= a school timetable) whether or not to shuffle one of its rows
-            if random.randrange(0, 1) < self.muatation_rate:
+            if random.randrange(0, 1) < self.mutation_rate:
                 teachers = input[1]
                 rand_num = random.randrange(0, 49)
                 random.shuffle(teachers)
